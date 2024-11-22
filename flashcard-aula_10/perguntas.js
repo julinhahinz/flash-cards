f40635237099b67cd4bdbcad86d658fb35958bfc
julinhahinz <161838646+julinhahinz@users.noreply.github.com>
2024-11-22 09:29:32 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,49 @@
+criaCartao(
+    'QUIZ',
+    'quanto 10x27+39x0',
+    '270'
+)
+
+criaCartao(
+    'QUIZ',
+    'que dia é a proclamação da republica do Brasil?',
+    '15 de novembro'
+)
+
+criaCartao(
+    'QUIZ',
+    'qual o maior orgão do corpo humano?',
+    'a pele'
+)
+
+criaCartao(
+    'QUIZ',
+    'qual o principal ingrediente da cerveja?',
+    'malte'
+)
+
+criaCartao(
+    'QUIZ' ,
+    'qual foi o primeiro presidente do Brasil?' ,
+    'Marechal Manuel Deodoro da Fonseca'
+)
+
+criaCartao(
+   'QUIZ' ,
+    'de quem é a famosa frase "PENSO, LOGO EXISTO"?' ,
+    'René Descartes'
+)
+
+criaCartao(
+    'QUIZ' ,
+    'quantos elementos quimicos existem atualmente na tabela periodica?' ,
+    '118'
+
+)
+
+criaCartao(
+    'QUIZ' ,
+    ' qual o maior animal terrestre?' ,
+    ' elefante africano'
+
+)
